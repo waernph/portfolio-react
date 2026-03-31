28260e6c5b64f957cf94e6c9ebd8a31c1589c440
Philip Waern <waernph@gmail.com>
2026-03-31 09:10:50 +0200

    Added href to navbar

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ function App() {
       <header className="App-header">
         <nav>
           <ul className="Navbar-ul">
-            <li className="Navbar-li">About Me</li>
-            <li className="Navbar-li">Skills</li>
-            <li className="Navbar-li">Portfolio</li>
-            <li className="Navbar-li">References</li>
+            <li className="Navbar-li"><a href="#about-me">About Me</a></li>
+            <li className="Navbar-li"> <a href="#skills">Skills</a></li>
+            <li className="Navbar-li"> <a href="#portfolio">Portfolio</a></li>
+            <li className="Navbar-li"> <a href="#references">References</a></li>
           </ul>
         </nav>
       </header>
-      <main>
-        <section className="Main">main</section>
+      <main className="Main">
+        <section id="about-me"><h2>About me</h2></section>
+        <section id="skills"><h2>Skills</h2></section>
+        <section id="portfolio"><h2>Portfolio</h2></section>
+        <section id="references"><h2>References</h2></section>
       </main>
       <footer className="Footer">
         <section>footer</section>
