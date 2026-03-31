4cb40ff2120387be43eebff53d00e8fba2d3e90e
Philip Waern <waernph@gmail.com>
2026-03-31 11:42:15 +0200

    Added content to skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,6 +1,67 @@
 function Skills() {
     return (
-        <section id="skills"><h2>Skills</h2></section>
+        <section id="skills">
+            <h2>Skills</h2>
+            <ul class="logos">
+                <li>
+                    <img src="resources/media/logos/C_Sharp_Logo_2023_cropped.svg" alt="logo of C#" />
+                    <ul class="skill-list">
+                        <li>OOP</li>
+                        <li>ASP.NET</li>
+                        <li>Entity Framework</li>
+                        <li>Web API</li>
+                        <li>AutoMapper</li>
+                        <li>Swagger/Postman</li>
+                        <li>JWT</li>
+                    </ul>
+                </li>
+                <li>
+                    <img src="resources/media/logos/Microsoft_SQL_Server_2025_icon.svg" alt="Microsoft SQL Server 2025 logo" />
+                        <ul class="skill-list">
+                            <li>SQL Server</li>
+                            <li>SQL Server Management Studio</li>
+                            <li>DDL/DML</li>
+                            <li>Subqueries</li>
+                            <li>Stored Procedures</li>
+                            <li>Transactions</li>
+                        </ul>
+                </li>
+                <li>
+                    <img id="html5-logo" src="resources/media/logos/HTML5_logo_and_wordmark_white.svg" alt="Logo of HTML5" />
+                    <ul class="skill-list">
+                        <li>Create a SPA</li>
+                        <li>Semantic HTML</li>
+                        <li>WCAG</li>
+                    </ul>
+                </li>
+                <li>
+                    <img id="css3-logo" src="resources/media/logos/CSS3_logo_and_wordmark_white.svg" alt="Logo of CSS3" />
+                    <ul class="skill-list">
+                        <li>CSS styling</li>
+                        <li>Animation</li>
+                        <li>CSS Pseudo-classes</li>
+                        <li>Flex / Grid</li>
+                    </ul>
+                </li>
+                <li>
+                    <img id="js-logo" src="resources/media/logos/Javascript-shield.svg" alt="Logo of JavaScript" />
+                    <ul class="skill-list">
+                        <li>Fetch</li>
+                        <li>JavaScript DOM</li>
+                        <li>Event listeners</li>
+                        <li>React</li>
+                    </ul>
+                </li>
+                <li>
+                    <img id="github-logo" src="resources/media/logos/GitHub/GitHub_Invertocat_White.svg" alt="Logo of GitHub" />
+                    <ul class="skill-list">
+                        <li>Git and GitHub Repositories</li>
+                        <li>Git CLI</li>
+                        <li>Pages</li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
     )
 }
 export default Skills;
