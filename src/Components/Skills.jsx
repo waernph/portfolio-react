fd9e777fd824b9a259f2dc679aade101762815b4
Philip Waern <waernph@gmail.com>
2026-03-31 12:09:38 +0200

    Added loggos and lists to skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,11 +1,17 @@
+import { ReactComponent as CSharpLogo } from '../Media/logos/C_Sharp_Logo_2023_cropped.svg'
+import { ReactComponent as CSS3Logo } from "../Media/logos/CSS3_logo_and_wordmark_white.svg"
+import { ReactComponent as HTML5Logo } from "../Media/logos/HTML5_logo_and_wordmark_white.svg"
+import { ReactComponent as JavaScriptLogo } from "../Media/logos/Javascript-shield.svg"
+import { ReactComponent as SQLServerLogo } from "../Media/logos/Microsoft_SQL_Server_2025_icon.svg"
+import { ReactComponent as GitHubLogo} from "../Media/logos/GitHub/GitHub_Invertocat_White.svg"
 function Skills() {
     return (
         <section id="skills">
             <h2>Skills</h2>
-            <ul class="logos">
+            <ul className="logos">
                 <li>
-                    <img src="resources/media/logos/C_Sharp_Logo_2023_cropped.svg" alt="logo of C#" />
-                    <ul class="skill-list">
+                    <CSharpLogo className="Logo-img" />
+                    <ul className="skill-list">
                         <li>OOP</li>
                         <li>ASP.NET</li>
                         <li>Entity Framework</li>
@@ -16,36 +22,38 @@ function Skills() {
                     </ul>
                 </li>
                 <li>
-                    <img src="resources/media/logos/Microsoft_SQL_Server_2025_icon.svg" alt="Microsoft SQL Server 2025 logo" />
-                        <ul class="skill-list">
-                            <li>SQL Server</li>
-                            <li>SQL Server Management Studio</li>
-                            <li>DDL/DML</li>
-                            <li>Subqueries</li>
-                            <li>Stored Procedures</li>
-                            <li>Transactions</li>
-                        </ul>
+                    {/* <SQLServer className="Logo-img"/> */}
+                    <ul className="skill-list">
+                        <li>SQL Server</li>
+                        <li>SQL Server Management Studio</li>
+                        <li>DDL/DML</li>
+                        <li>Subqueries</li>
+                        <li>Stored Procedures</li>
+                        <li>Transactions</li>
+                    </ul>
                 </li>
                 <li>
-                    <img id="html5-logo" src="resources/media/logos/HTML5_logo_and_wordmark_white.svg" alt="Logo of HTML5" />
-                    <ul class="skill-list">
+                    <HTML5Logo className='Logo-img' />
+                    <ul className="skill-list">
                         <li>Create a SPA</li>
                         <li>Semantic HTML</li>
                         <li>WCAG</li>
                     </ul>
                 </li>
                 <li>
+                    {/* <CSS3Logo /> */}
                     <img id="css3-logo" src="resources/media/logos/CSS3_logo_and_wordmark_white.svg" alt="Logo of CSS3" />
-                    <ul class="skill-list">
+                    <ul className="skill-list">
                         <li>CSS styling</li>
                         <li>Animation</li>
-                        <li>CSS Pseudo-classes</li>
+                        <li>CSS Pseudo-classNamees</li>
                         <li>Flex / Grid</li>
                     </ul>
                 </li>
                 <li>
+                    {/* <JavaScriptLogo /> */}
                     <img id="js-logo" src="resources/media/logos/Javascript-shield.svg" alt="Logo of JavaScript" />
-                    <ul class="skill-list">
+                    <ul className="skill-list">
                         <li>Fetch</li>
                         <li>JavaScript DOM</li>
                         <li>Event listeners</li>
@@ -54,7 +62,7 @@ function Skills() {
                 </li>
                 <li>
                     <img id="github-logo" src="resources/media/logos/GitHub/GitHub_Invertocat_White.svg" alt="Logo of GitHub" />
-                    <ul class="skill-list">
+                    <ul className="skill-list">
                         <li>Git and GitHub Repositories</li>
                         <li>Git CLI</li>
                         <li>Pages</li>
