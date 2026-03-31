66eb06513d4f021f86b7b2d17a9ecaa807673b7f
Philip Waern <waernph@gmail.com>
2026-03-31 08:20:53 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="Navbar">navbar</section>
       </header>
+      <main>
+        <section className="Main">main</section>
+      </main>
+      <footer className="Footer">
+        <section>footer</section>
+      </footer>
     </div>
   );
 }
