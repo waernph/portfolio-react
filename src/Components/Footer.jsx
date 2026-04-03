1e5291a4a36dd5ddb2b487d5a3de237c51837f84
Philip Waern <waernph@gmail.com>
2026-04-03 21:40:32 +0200

    Added font awesome but not showing. Need fix. Fixed styling on reference

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faUnsplash } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, far, fab)
+
 function Footer() {
     return (
         <section>
-            <ul>
+            <ul className="footerList">
                 <li>© Philip Waern {new Date().getFullYear()}</li>
-                <li><a href="https://github.com/waernph" target="_blank" aria-label="Link to GitHub"><i
-                    class="fa-brands fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/philip-w%C3%A6rn/" target="_blank" aria-label="Link to LinkedIn"><i
-                        class="fa-brands fa-linkedin"></i></a>
-                    <a href="https://unsplash.com/@waern" target="_blank" aria-label="Link to Unsplash"><i
-                        class="fa-brands fa-unsplash"></i></a>
-                </li>
+                <li><a href="https://github.com/waernph" rel="noreferrer" target="_blank" aria-label="Link to GitHub"><FontAwesomeIcon icon={['fab', 'github']} /></a></li>
+                <li><a href="https://www.linkedin.com/in/philip-w%C3%A6rn/" rel="noreferrer" target="_blank" aria-label="Link to LinkedIn"><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></a></li>
+                <li><a href="https://unsplash.com/@waern" rel="noreferrer" target="_blank" aria-label="Link to Unsplash"><FontAwesomeIcon icon={faUnsplash} /></a></li>
             </ul>
-
         </section>
     )
 }
