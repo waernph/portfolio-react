d71ddb97a6363054d09de9ed839088cb05d8422a
Philip Waern <waernph@gmail.com>
2026-04-03 20:40:26 +0200

    Added year function and links in ul

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,18 @@
 function Footer() {
-    return(
-        <section>footer</section>
+    return (
+        <section>
+            <ul>
+                <li>© Philip Waern {new Date().getFullYear()}</li>
+                <li><a href="https://github.com/waernph" target="_blank" aria-label="Link to GitHub"><i
+                    class="fa-brands fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/philip-w%C3%A6rn/" target="_blank" aria-label="Link to LinkedIn"><i
+                        class="fa-brands fa-linkedin"></i></a>
+                    <a href="https://unsplash.com/@waern" target="_blank" aria-label="Link to Unsplash"><i
+                        class="fa-brands fa-unsplash"></i></a>
+                </li>
+            </ul>
+
+        </section>
     )
 }
 export default Footer;
