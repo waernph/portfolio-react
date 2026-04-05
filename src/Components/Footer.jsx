d0ad878582778df9108f3f9872c27b63150364a7
Philip Waern <waernph@gmail.com>
2026-04-05 10:32:59 +0200

    Cleaned up footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,27 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-/* import all the icons in Free Solid, Free Regular, and Brands styles */
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faUnsplash } from '@fortawesome/free-brands-svg-icons'
-library.add(fas, far, fab)
+library.add(fab)
 
 function Footer() {
     return (
         <section className='footerSection'>
             <p className='copyrightInfo'>© Philip Waern {new Date().getFullYear()}</p>
             <div className='footerLogos'>
-                <a href="https://www.linkedin.com/in/philip-w%C3%A6rn/" target='_blank'>
+                <a href="https://www.linkedin.com/in/philip-w%C3%A6rn/" target='_blank' rel="noreferrer">
                     <FontAwesomeIcon className='faIcon' icon="fa-brands fa-linkedin" />
                 </a>
 
-                <a href='https://github.com/waernph' target='_blank'>
+                <a href='https://github.com/waernph' target='_blank' rel="noreferrer">
                     <FontAwesomeIcon className='faIcon' icon="fa-brands fa-square-github" />
                 </a>
 
-                <a href={"https://unsplash.com/@waern"} target='_blank'>
+                <a href={"https://unsplash.com/@waern"} target='_blank' rel="noreferrer">
                     <FontAwesomeIcon className='faIcon' icon="fa-brands fa-unsplash" />
                 </a>
 
