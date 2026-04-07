87436d74c0d4650ab24c94f2356edc5f293a62b1
Philip Waern <waernph@gmail.com>
2026-04-07 11:15:22 +0200

    Added som more styling to card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,14 +6,16 @@ library.add(fab)
 
 
 
-function Card({ title, descripton, imgUrl }) {
+function Card({ title, descripton, imgUrl, gitHubUrl, testMeUrl }) {
     return (
         <div className='Card'>
             <h3>{title}</h3>
             <img src={imgUrl} alt="image of project" />
             <p>{descripton}</p>
-            <button>GitHub</button>
-            <button>Try it!</button>
+            <div className='BtnSection'>
+                <a href={gitHubUrl}><button>GitHub</button></a>
+                <a href={testMeUrl}><button>Try it!</button></a>
+            </div>
         </div>
     )
 }
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -5,7 +5,7 @@ function Portfolio() {
         <section id="portfolio">
             <h2>Portfolio</h2>
             <div className='Projects'>
-                <Card title={"Shotgun"} descripton={"Text här"} imgUrl={""} />
+                <Card title={"Keybaord"} descripton={"My own project in trying to make a keyboard using HTML, CSS, JavaScript and Web Audio API."} imgUrl={""} gitHubUrl={"www.github.com"} testMeUrl={"wrn.se"} />
                 <Card />
                 <Card />
                 <Card />
