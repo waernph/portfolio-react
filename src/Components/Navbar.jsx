5ec4e4f99dd50f05bcbc1b95c71effc25392c863
Philip Waern <waernph@gmail.com>
2026-04-08 13:11:25 +0200

    added smooth scrolling, scroll margin topp

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
   return (
     <nav>
       <ul className="Navbar-ul">
-        <li className="Navbar-li"><a className="Navbar-li-a" href="#"><DanishAe className="NavbarLogo" /></a></li>
+        <li className="Navbar-li"><a href="#"><DanishAe className="NavbarLogo" /></a></li>
         <li className="Navbar-li"><a className="Navbar-li-a" href="#about-me">About Me</a></li>
         <li className="Navbar-li"><a className="Navbar-li-a" href="#skills">Skills</a></li>
         <li className="Navbar-li"><a className="Navbar-li-a" href="#portfolio">Portfolio</a></li>
