bf5402057482a0840a16820dc82079c0ac24314a
Philip Waern <waernph@gmail.com>
2026-04-08 13:12:09 +0200

    Removed lorem references

--- src/Components/Functions/ReferencesTest.jsx
+++ src/Components/Functions/ReferencesTest.jsx
@@ -4,16 +4,6 @@ let refDict = [
         "name": "Jakob Rockmyr",
         "text": "Philip är en otroligt uppskattad kollega som lyfter hela teamet. Han kombinerar stark teknisk kompetens med ett kreativt tänk som märks i allt han gör.",
         "company": "Owner, BST"
-    },
-    {
-        "name": "Jenny Spetz",
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo, voluptatem eligendi quia rem minima laudantium esse suscipit quod aut.",
-        "company": "Art Director, Liseberg"
-    },
-    {
-        "name": "Mattias Fredriksson",
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo, voluptatem eligendi quia rem minima laudantium esse suscipit quod aut.",
-        "company": "Creative Director, Maverick"
     }
 ]
 
